9e4adb698c7ef9eafbe82e6d5d8e39f5b6f1752c
Federico Nafria <federiconafria@gmail.com>
2024-05-24 09:10:05 +0200

    Move golang sources

---
+++ golang-apiserver/cmd/api/routes.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/alexedwards/flow"
+)
+
+func (app *application) routes() http.Handler {
+	mux := flow.New()
+
+	mux.NotFound = http.HandlerFunc(app.notFound)
+	mux.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowed)
+
+	mux.Use(app.recoverPanic)
+
+	mux.HandleFunc("/status", app.status, "GET")
+	mux.HandleFunc("/", app.root, "GET")
+
+	return mux
+}
